ee3cba3c9e87a281fcae2761a35d83979350ac7c
Mutter <mutter8525@yandex.ru>
2024-03-04 15:41:10 +0300

    My project

---
+++ Diplom/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Diplom.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+      [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Длина может быть от 1 до 1000")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+
+
+    }
+}
